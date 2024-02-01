ee8ba06bd900532fa7effd41dff4fb3e98ea6f36
Val-d-emar <vhot@live.ru>
2024-02-02 00:22:14 +0400

    feat: up done

--- src/index.js
+++ src/index.js
@@ -1,21 +1,44 @@
 import { createWriteStream } from 'node:fs';
 import process from 'node:process';
 import { fileURLToPath } from 'node:url';
-import { dirname, normalize } from 'node:path';
+import path from 'node:path';
 import { EOL } from 'node:os';
 // import EventEmitter from 'node:events';
-const dir = dirname(fileURLToPath(import.meta.url));
+let path_to_working_directory = path.normalize(path.dirname(fileURLToPath(import.meta.url))).toString();
+console.log(path_to_working_directory);
 
-let emitErr = (err) => {
-    process.stdin.emit(
-        'error',
-        new Error(err)
-    );
+let emitErr = (err) => { process.stdin.emit('error', new Error(err)); };
+
+let emitDir = (dir) => { process.stdin.emit('dir', dir); };
+
+let dir2array = (path_to = path_to_working_directory) => {
+    let res = path_to.split(path.sep);
+    if (res.length > 0 && res[0] === '') {
+        res[0] = '/';
+    }
+    return res;
+}
+
+let array_working_directory = dir2array(path_to_working_directory);
+
+let dirUp = (path_to = path_to_working_directory) => {
+    let res = path_to.split(path.sep);
+    if (res.length > 1) {
+        res.pop();
+    }    
+    if (res.length == 1 && res[0] === '') {
+        res[0] = '/';
+    }
+    array_working_directory = res;
+    console.log(array_working_directory);
+    path_to_working_directory = array_working_directory.join(path.sep);
+    return res;
 }
 
 const read_tty = async () => {
     // Write your code here 
 
+    console.log(array_working_directory);
     let currentOperation = '';
     let username = 'Anonymous';
     if (process.argv.length > 2) {
@@ -30,25 +53,36 @@ const read_tty = async () => {
     };
 
     process.stdout.write(`Welcome to the File Manager, ${username}!${EOL}`);
+    process.stdout.write(`You are currently in ${path_to_working_directory}${EOL}`);
 
     process.stdin
         .on('data', data => {
             if (data.toString().startsWith('.exit')) {
                 process.exit(0);
+            } else if (data.toString().includes('up')) {
+                process.stdout.write(`Operation: ${data}`);
+                dirUp();
+                currentOperation = data.toString().trim();
+                emitDir(path_to_working_directory);
             } else if (data.toString().includes('ls')) {
                 process.stdout.write(`Operation: ${data}`);
                 currentOperation = data.toString().trim();
-                emitErr(`Operation failed: ${currentOperation}${EOL}`);
+                // emitErr(`Operation failed: ${currentOperation}${EOL}`);
+                emitDir(path_to_working_directory);
             } else {
                 emitErr(`Invalid input: ${data}`);
             }
+
+        })
+        .on('dir', dir => {
+            process.stdout.write(`You are currently in ${dir}${EOL}`);
         })
         .on('error', err => {
             process.stdout.write(err.message);
-        })
-        ;
+        });
+
     process.prependOnceListener('exit', code => {
-        process.stdout.write(`Thank you for using File Manager, ${username}, goodbye!${EOL}`);
+        process.stdout.write(`${EOL}Thank you for using File Manager, ${username}, goodbye!${EOL}`);
         return code;
     });
     process.prependOnceListener("SIGINT", code => {
